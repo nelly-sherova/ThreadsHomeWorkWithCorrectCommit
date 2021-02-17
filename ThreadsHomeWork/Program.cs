3b68c0e284e7764f748cb78f29b0fef24537fe43
unknown <nellysherova@mail.ru>
2021-02-17 16:14:30 +0300

    Начало работы

---
+++ ThreadsHomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThreadsHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
